035be8765e7a7d748a126bc518b61140ac728afd
Fernando Labastida <labastidafernando460@gmail.com>
2022-10-09 19:13:17 -0500

    Página mousemove

---
+++ index.js
@@ -0,0 +1,12 @@
+let si = document.getElementById("si");
+si.addEventListener("click", function(e){
+    alert("Gracias bb");
+} );
+
+let no=document.getElementById("no");
+no.addEventListener("mousemove", function(e){
+    let x = Math.round(Math.random()*93);
+    let y = Math.round(Math.random()*93);
+    no.style.left = x+"%";
+    no.style.top = y+"%";
+} );
